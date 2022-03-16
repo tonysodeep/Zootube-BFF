89c350eb8c744c23f18ee9b6d90d03ea4673d19f
tonysodeep <hynguyen2727@gmail.com>
2022-03-16 20:37:57 +0700

    add file to server temp

--- routes/users-route.js
+++ routes/users-route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { check } = require('express-validator');
 
 const usersController = require('../controllers/users-controller');
+const fileUpload = require('../middleware/file-upload');
 
 const router = express.Router();
 
@@ -9,6 +10,7 @@ router.get('/', usersController.getUsers);
 
 router.post(
   '/signup',
+  fileUpload.single('image'),
   [
     [check('email').isEmail().normalizeEmail().trim()],
     check('username').not().isEmpty().trim(),
